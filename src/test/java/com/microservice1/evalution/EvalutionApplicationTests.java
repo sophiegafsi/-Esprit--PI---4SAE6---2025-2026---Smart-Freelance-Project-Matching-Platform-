f9a03ce05feb1aa227c45ad80faa0ffb2f99a5ca
hajerKhazri <Khazri.hajer@esprit.tn>
2026-02-12 01:08:26 +0100

    commit 1

---
+++ src/test/java/com/microservice1/evalution/EvalutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice1.evalution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
