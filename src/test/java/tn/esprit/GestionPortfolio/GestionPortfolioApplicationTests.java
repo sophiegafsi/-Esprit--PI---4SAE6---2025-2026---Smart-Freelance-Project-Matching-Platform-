ef914b049fde88f853544926408778bb31584119
fares-belga <fares.belgacem@esprit.tn>
2026-04-23 10:44:47 +0100

    test(ci): use test profile and H2 for portfolio pipeline

--- src/test/java/tn/esprit/GestionPortfolio/GestionPortfolioApplicationTests.java
+++ src/test/java/tn/esprit/GestionPortfolio/GestionPortfolioApplicationTests.java
@@ -2,8 +2,10 @@ package tn.esprit.GestionPortfolio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GestionPortfolioApplicationTests {
 
 	@Test
