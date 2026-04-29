74a7c05f617aad6b887a51756f0fce1a879f0f49
allouchimaha <129538920+allouchimaha@users.noreply.github.com>
2026-04-30 00:27:26 +0200

    Add SpringBootTest annotation to test class

--- src/test/java/com/example/projet_service/ProjetServiceApplicationTests.java
+++ src/test/java/com/example/projet_service/ProjetServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.projet_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest
 class ProjetServiceApplicationTests {
 
     @Test
