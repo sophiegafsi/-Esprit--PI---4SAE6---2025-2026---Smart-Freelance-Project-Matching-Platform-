855201dbcb2b58c4de40d127c1e0ac3f68890b4a
allouchimaha <129538920+allouchimaha@users.noreply.github.com>
2026-04-30 00:33:06 +0200

    Remove SpringBootTest annotation from test class
    Removed SpringBootTest annotation from tests.

--- src/test/java/com/example/projet_service/ProjetServiceApplicationTests.java
+++ src/test/java/com/example/projet_service/ProjetServiceApplicationTests.java
@@ -1,11 +1,9 @@
 package com.example.projet_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
 class ProjetServiceApplicationTests {
 
     @Test
