ec41a2acb035e10795e208c9f0a3bdab3f9f3300
mohamedali <mohamedali.romdhane@esprit.tn>
2026-05-05 19:26:55 +0100

    fix reservation tests jwt decoder

--- reservation-service/src/test/java/tn/esprit/reservation/NotificationTest.java
+++ reservation-service/src/test/java/tn/esprit/reservation/NotificationTest.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.reservation.repositories.NotificationRepository;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 public class NotificationTest {
+
+    @MockBean
+    private JwtDecoder jwtDecoder;
 
     @Autowired
     private NotificationRepository notificationRepository;
