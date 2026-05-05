1b35ae5acd2d07b474d6a40f5d1eff3c621b4b31
mohamedali <mohamedali.romdhane@esprit.tn>
2026-05-05 22:15:27 +0100

    fix user CI: disable security in tests

--- user/src/test/java/freelink/user/UserApplicationTests.java
+++ user/src/test/java/freelink/user/UserApplicationTests.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 class UserApplicationTests {
 
     @MockBean
