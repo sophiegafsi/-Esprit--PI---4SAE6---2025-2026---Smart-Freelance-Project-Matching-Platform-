76178e3218f69ef15207ed44dbafdb20ed605a42
mohamedali <mohamedali.romdhane@esprit.tn>
2026-05-05 22:19:03 +0100

    fix user CI: disable oauth2 auto-config in tests

--- user/src/test/java/freelink/user/UserApplicationTests.java
+++ user/src/test/java/freelink/user/UserApplicationTests.java
@@ -9,9 +9,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.context.annotation.Import;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 @SpringBootTest
 @ActiveProfiles("test")
 @Import(TestSecurityConfig.class)
+@EnableAutoConfiguration(exclude = {
+    org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration.class
+})
 class UserApplicationTests {
 
     @MockBean
